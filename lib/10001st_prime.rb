2e4b2a9b1283e40cf2f2ba854190e38a703ffe72
boom100100 <bsd231@nyu.edu>
2020-09-17 15:42:14 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+
+# # NOTE: could use native Prime class here, but that obviously defeats the purpose of this exercise
+def prime_number_for(nth_element)
+  primes = [2]
+  i = 3
+  while primes.length < nth_element
+    is_prime = primes.none?{|n| i % n == 0}
+    primes.push(i) if is_prime
+    i += 1
+  end
+  primes.pop()
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  def initialize(nth_element)
+    @nth_element = nth_element
+  end
+  def number
+    primes = [2]
+    i = 3
+    while primes.length < @nth_element
+      is_prime = primes.none?{|n| i % n == 0}
+      primes.push(i) if is_prime
+      i += 1
+    end
+    primes.pop()
+  end
+end
